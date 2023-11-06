28a44254667d233f3c194d63421177ca91a17f15
Quazi Samiha <quazisamiha@gmail.com>
2023-11-06 12:49:04 -0800

    get and post operation performed

--- index.js
+++ index.js
@@ -2,38 +2,42 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-// middleware
+
+// middleware adding
 app.use(cors());
 app.use(express.json());
 
 const users = [
   {
     id: 1,
-    name: "John Doe",
-    email: "john@example.com",
+    userName: "John Doe",
+    userEmail: "john@example.com",
   },
   {
     id: 2,
-    name: "Jane Smith",
-    email: "jane@example.com",
+    userName: "Jane Smith",
+    userEmail: "jane@example.com",
   },
   {
     id: 3,
-    name: "Bob Johnson",
-    email: "bob@example.com",
+    userName: "Bob Johnson",
+    userEmail: "bob@example.com",
   },
 ];
 
-app.get("/", (req, res) => {
-  res.send("User Management server is running");
+app.get("./", (req, res) => {
+  res.send("User Management Server is running");
 });
 
+// all users fetched from server-------------------
 app.get("/users", (req, res) => {
   res.send(users);
 });
 
+// new user added to server----------------------
 app.post("/users", (req, res) => {
-  //   console.log(req.body);
+  // console.log(req);
+  // console.log(req.body);
   const newUser = req.body;
   newUser.id = users.length + 1;
   users.push(newUser);
@@ -41,5 +45,5 @@ app.post("/users", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`server is running on PORT: ${port}`);
+  console.log(`server is running one PORT: ${port}`);
 });
